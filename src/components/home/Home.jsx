a5e0187054272944e0226c676dc4cfcab1074114
MuneebShah0 <muneebwali809@gmail.com>
2023-11-19 12:21:52 +0500

    css

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
         <>
 
             <div className='flex justify-evenly main items-center gap-3 rounded-2xl p-[1.875rem] mt-12 mx-10 bg-gray-100'>
-                <div className='flex flex-col p-2'>
+                <div className='flex flex-col p-2 innermain'>
                     {/* include 5 words only in first h1 and in second 5*/}
                     <h1 className={` font-extrabold text-[4.8vw] leading-[1] mb-3 text-gray-700 `}>Musk Shocks the World</h1>
                     <h1 className={` font-extrabold text-[2.8vw] mb-2 text-gray-600`}>Apple's Company Up for Sale!</h1>
