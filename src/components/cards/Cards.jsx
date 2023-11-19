9f7c6ca1f7b1b90b777d47f65db6b5e18e98fed6
MuneebShah0 <muneebwali809@gmail.com>
2023-11-19 12:26:40 +0500

    image

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react'
 import "./cards.css"
 import Link from 'next/link'
 import data from '@/api'
+import Image  from "next/image"
 
 const Cards = () => {
     const [showMore, setShowMore] = useState(false)
@@ -16,7 +17,7 @@ const Cards = () => {
                 {
                     vissibleCards.map((d) => (
                         <div key={d.id} className="flex cardsbody   gap-4 flex-col justify-center w-full max-w-[251px] mb-4 pb-1 border border-solid border-2 shadow-2xl  border-grey-600 rounded-xl">
-                            <img className="rounded-t-[0.3rem] h-[26vh] max-w-sm  hover:transform object-cover" src='/c.jpg' alt="" />
+                            <Image width={250} height={250} quality={50} className="rounded-t-[0.3rem] h-[26vh] max-w-sm  hover:transform object-cover" src='/c.jpg' alt="" />
                             <div className="flex flex-col gap-3 px-3 pb-3 ">
                                 <h3 className="font-bold text-[1.1rem] opacity-90  text-gray-700">{d.title && `${d.title.slice(0, 69)} ${d.title.length > 69 ? "...." : ''}`}..</h3>
                                 <p className='text-gray-600'>{d.tagLine && `${d.tagLine.slice(0, 57)} ${d.tagLine.length > 57 ? "...." : ''}`}.</p>
@@ -31,7 +32,7 @@ const Cards = () => {
             </div>
 
             <div className="flex justify-center mb-8">
-                <button className='rounded-full shadow-2xl px-4 py-2  hover:bg-transparent border border-gray-400 transition-all duration-500' onClick={()=> setShowMore(!showMore)}>
+                <button className='rounded-full shadow-2xl px-4 py-2  hover:bg-transparent border border-gray-400 transition-all duration-500' onClick={() => setShowMore(!showMore)}>
                     {showMore ? "Show Less" : "Show More"}
                 </button>
             </div>
